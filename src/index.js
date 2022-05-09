1d6181d9a3f5c78d440e6c623e81501492529f25
Kirtan-G <kirtan942003@gmail.com>
2022-05-09 16:38:57 +0530

    Kirtan Gajjar

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const route = require('./routes/route');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://uranium:uranium@cluster0.pgmlm.mongodb.net/group49Database").then(() => console.log("MongoDB Is Connected")).catch(err => console.log(err));
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express App Is Running On Port' + (process.env.PORT || 3000))
+});
