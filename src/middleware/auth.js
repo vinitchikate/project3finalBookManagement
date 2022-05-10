0269def745756efc58856e2eda54c89bfa78ca40
prem9528 <101805758+prem9528@users.noreply.github.com>
2022-05-10 23:06:58 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,7 +7,12 @@ const authentication = async function (req, res, next) {
                 status: false,
                 msg: "Token required! Please login to generate token",
             });
-
+        let tokenValidity = jwt.decode(token, "bookM49");
+        let tokenTime = (tokenValidity.expiresIn) * 1000;
+        let CreatedTime = Date.now()
+        if (CreatedTime > tokenTime){
+        return res.status(400).send({status: false, msg: "token is expired, login again"})
+        }
         let decodedToken = jwt.verify(token, "bookM49");
         if (!decodedToken)
             return res.status(401).send({ status: false, msg: "token is invalid" });
