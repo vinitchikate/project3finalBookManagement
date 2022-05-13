58ebabc0c3dc68211d20d87735814b71887913fd
Kirtan-G <kirtan942003@gmail.com>
2022-05-13 16:28:48 +0530

    Kirtan Gajjar

--- src/controller/reviewController.js
+++ src/controller/reviewController.js
@@ -1,4 +1,5 @@
- const reviewModel = require('../models/reviewModel')
+const reviewModel = require('../models/reviewModel');
+const bookModel = require('../models/booksModel');
 
 const createreview = async function (req, res) {
     try {
@@ -17,4 +18,37 @@ const createreview = async function (req, res) {
     }
 };
 
-module.exports = {createreview};
+
+const dreview = async function (req, res) {
+    try {
+        let reviewId = req.params.reviewId;
+        let findreviewId = await reviewModel.findById(reviewId);
+        if (!findreviewId) {
+            return res.status(404).send({ status: false, msg: "Review Id Not Found, Plz Enter Valid ReviewId" });
+        }
+        if (findreviewId.isDeleted == true) {
+            return res.status(400).send({ status: false, msg: "Review Is Already Deleted" });
+        }
+
+        let bookId = req.params.bookId;
+        let findbookId = await bookModel.findById(bookId);
+        if (!findbookId) {
+            return res.status(404).send({ status: false, msg: "Book Id Not Found, Plz Enter Valid BookId" });
+        }
+        
+        let uReview = await reviewModel.findOneAndUpdate({ _id: reviewId }, { isDeleted: true }, { new: true });
+        let findb = await bookModel.findById(bookId);
+        let decrease = (findb.reviews) - 1;
+        console.log(decrease)
+        let updateCount = await bookModel.findOneAndUpdate({ _id: bookId }, { reviews: decrease });
+
+        res.status(200).send({ status: true, data: uReview });
+    }
+    catch (err) {
+        res.status(500).send({ status: false, msg: err.message });
+    }
+}
+
+
+
+module.exports = { createreview, dreview };
--- src/routes/route.js
+++ src/routes/route.js
@@ -20,9 +20,9 @@ router.put("/books/:bookId", auth.authentication, auth.authorization, booksContr
 router.delete("/books/:bookId", auth.authentication, booksController.deleteBooks);
 
 // Review APIs
-router.post("/books/:bookId/review", valid.validreview , reviewController.createreview);
+router.post("/books/:bookId/review", valid.validreview, reviewController.createreview);
 // router.put("/books/:bookId/review/:reviewId");
-// router.delete("/books/:bookId/review/:reviewId");
+router.delete("/books/:bookId/review/:reviewId", reviewController.dreview);
 
 
 module.exports = router;
