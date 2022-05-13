ae0405d34761d1163ac881b4cafd89a17c1c1e65
parteek jain <102279288+parteekjain99@users.noreply.github.com>
2022-05-13 11:29:30 +0530

    Update reviewController.js

--- src/controller/reviewController.js
+++ src/controller/reviewController.js
@@ -1,1 +1,18 @@
-//
+ const reviewModel = require('../Model/reviewModel')
+
+const createreview = async function (req, res) {
+    try {
+        const review = req.body;
+        let requestedUserId = req.params.bookId;
+        let bodyUser = review.bookId;
+        if (requestedUserId !== bodyUser) {
+            return res.status(401).send({ status: false, msg: "review is not present" });
+        }
+
+        const saved = await reviewModel.create(review);
+        res.status(201).send({ status: true, msg: "review is created successfully.", data: saved });
+    }
+    catch (err) {
+        res.status(500).send({ msg: err.message });
+    }
+};
